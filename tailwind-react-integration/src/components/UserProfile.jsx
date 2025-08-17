f537386f7a2aa9e8696845bac55e677d135d9d62
kernemi kidane <kernemikidane63@gmail.com>
2025-08-17 22:48:29 +0300

    "editing"

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,10 +4,10 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
+        className="sm:w-24 sm:h-24 w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-3 md:my-4 text-center hover:text-blue-500 transition-colors duration-300">
-        kernemi kidane
+        Kernemi Kidane
       </h1>
       <p className="text-gray-600 text-sm md:text-base text-center">
         Developer at AASTU. Loves to write code and explore new technologies.
